52281155cdee5d904a2874bdc4d32b3b108c76a6
BelkanWar <challenger1501@gmail.com>
2026-09-18 11:02:26 +0800

    Revert "nothing"
    This reverts commit 918e32109608fb42e9bcf2cff486059c9b7a73c1.

--- docker_gpu_setting.sh
+++ docker_gpu_setting.sh
@@ -5,10 +5,10 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-sudo apt update
+sudo apt-get update
 
 export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.20.0-1
-  sudo apt install -y \
+  sudo apt-get install -y \
       nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
       nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
       libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
